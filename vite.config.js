c599540b3c953785988f2a54525d6bcad991abf6
Nahuel <nahuelfaridrosales@gmail.com>
2024-12-26 04:22:04 -0300

    try

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/caminando-web/",
+  base: "https://nahuelrosales.github.io/caminando-web/",
   plugins: [react()],
 })
